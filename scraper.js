6da82ad7661c4dd59aec2f39e7f497340b7b9145
Joshua Stauter <jstauter@jungroup.com>
2016-03-23 19:19:28 -0400

    scraper works in theory...

--- scraper.js
+++ scraper.js
@@ -12,18 +12,19 @@ http.get({
 
     var redisClient = redis.createClient(process.env.REDIS_URL);
     redisClient.get('body', function (err, reply) {
+      if (err) console.log(err);
       if (body !== reply) {
         redisClient.set('body', body);
 
         pg.connect(process.env.DATABASE_URL, function (err, pgClient, done) {
-
+          if (err) console.log(err);
           var x = Math.floor(Date.now() / 1000), completed = 0;
 
           JSON.parse(body).group.entries.forEach(function (entry) {
-            console.log(pgClient);
             pgClient.query('INSERT INTO ranks (entryName, x, y) VALUES ($1, $2, $3)', [
               entry.entryName, x, entry.rank
             ], function (err, result) {
+              if (err) console.log(err);
               if (++completed === 26) done();
             });
           });
